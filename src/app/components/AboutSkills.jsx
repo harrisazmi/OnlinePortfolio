e04a723b69c4097aeb2df1b4272c791323e8466f
harrisazmi <93296522+harrisazmi@users.noreply.github.com>
2024-03-17 23:37:18 +0900

    uploaded pics

--- src/app/components/AboutSkills.jsx
+++ src/app/components/AboutSkills.jsx
@@ -1,85 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import TabButton from "./TabButton";
 
-const TAB_DATA = [
-  {
-    title: "Language Skills",
-    id: "skills",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>JavaScript</li>
-        <li>C++</li>
-        <li>Python</li>
-        <li>Pascal</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Education",
-    id: "education",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>
-          Bachelor Degree of Engineering (Hons) Chemical Engineering
-          <br />
-          Majoring in Gas Processing and Petrochemical
-          <br />
-          Petronas University Of Technology
-        </li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>
-          Cloud Practitioner (Entry Level) Runcloud Professional Certification
-        </li>
-        <li>Data Engineering by Yayasan Peneraju</li>
-        <li>MERN Stack Bootcamp by KelasProgramming</li>
-        <li>Google Cloud Big Data and Machine Learning Fundamentals</li>
-        <li>Crash Course on Python by Google</li>
-      </ul>
-    ),
-  },
-];
-
-const AboutSection = () => {
-  const [tab, setTab] = useState("skills");
-
-  const handleTabChange = (id) => {
-    setTab(id);
-  };
-
+const AboutSkills = () => {
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/About.png" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">Full stack web developer</p>
-          <div className="flex flex-row justify-start mt-8">
-            {TAB_DATA.map((item) => (
-              <TabButton
-                key={item.id}
-                selectTab={() => handleTabChange(item.id)}
-                active={tab === item.id}
-              >
-                {item.title}
-              </TabButton>
-            ))}
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab)?.content}
-          </div>
-        </div>
-      </div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Skills
+      </h2>
     </section>
   );
 };
 
-export default AboutSection;
+export default AboutSkills;
