4bbcc38bccf1af1261d92de10f82ab0dfe689196
harrisazmi <93296522+harrisazmi@users.noreply.github.com>
2024-03-18 01:50:33 +0900

    updated v2

--- src/app/components/AboutSkills.jsx
+++ src/app/components/AboutSkills.jsx
@@ -1,72 +1,59 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 
-const LogoContainer = ({ children }) => {
+const LogoContainer = ({ index, category, src }) => {
   return (
-    <div className="flex-none w-1/7 mx-4">
-      <div className="m-4 p-4 bg-gray-800 rounded-lg shadow-md">{children}</div>
+    <div
+      key={`${category}-${index}`}
+      className="m-4 p-4 bg-gradient-to-br from-primary-700 to-secondary-500 rounded-lg shadow-md"
+    >
+      <Image src={src} alt={`Logo ${index + 1}`} width={80} height={80} />
+    </div>
+  );
+};
+
+const CategoryBox = ({ name, count, dir }) => {
+  return (
+    <div className="m-4 p-6 rounded-lg shadow-md text-center bg-gradient-to-br from-primary-900 to-secondary-700">
+      <h3 className="text-lg font-bold text-gray-800 mb-4">{name}</h3>
+      <div className="flex flex-wrap justify-center">
+        {Array(count)
+          .fill()
+          .map((_, index) => (
+            <LogoContainer
+              key={index}
+              index={index}
+              category={name}
+              src={`/logos/${dir}/${index + 1}.png`}
+            />
+          ))}
+      </div>
     </div>
   );
 };
 
 const AboutSkills = () => {
-  const [logosPerRow, setLogosPerRow] = useState(4); // Initial number of logos per row
-
-  useEffect(() => {
-    const calculateLogosPerRow = () => {
-      const screenWidth = window.innerWidth;
-      if (screenWidth >= 1280) {
-        setLogosPerRow(7);
-      } else if (screenWidth >= 1024) {
-        setLogosPerRow(4);
-      } else if (screenWidth >= 768) {
-        setLogosPerRow(3);
-      } else {
-        setLogosPerRow(2);
-      }
-    };
-
-    calculateLogosPerRow();
-
-    const handleResize = () => {
-      calculateLogosPerRow();
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const numberOfLogos = 36; // Total number of logos
-
-  const rows = Math.ceil(numberOfLogos / logosPerRow);
-
-  const logoArray = [...Array(numberOfLogos).keys()];
+  const categories = [
+    { name: "Frontend", count: 6, dir: "frontend" },
+    { name: "Backend", count: 5, dir: "backend" },
+    { name: "Dev Ops", count: 12, dir: "devops" },
+    { name: "Others", count: 5, dir: "others" },
+  ];
 
   return (
     <section className="text-white" id="about">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+      <h2 className="text-center text-4xl font-bold text-gray-800 mt-8 mb-4 md:mb-8">
         My Skills
       </h2>
-      <div className="flex flex-wrap justify-center items-center">
-        {[...Array(rows).keys()].map((rowIndex) => (
-          <div key={rowIndex} className="flex w-full justify-center">
-            {logoArray
-              .slice(rowIndex * logosPerRow, (rowIndex + 1) * logosPerRow)
-              .map((index) => (
-                <LogoContainer key={index}>
-                  <Image
-                    src={`/logos/${index + 1}.png`}
-                    alt={`Logo ${index + 1}`}
-                    width={80}
-                    height={80}
-                  />
-                </LogoContainer>
-              ))}
-          </div>
+      <div className="flex flex-wrap justify-center">
+        {categories.map((category, index) => (
+          <CategoryBox
+            key={index}
+            name={category.name}
+            count={category.count}
+            dir={category.dir}
+          />
         ))}
       </div>
     </section>
